d8d83db49302c57764f3b6d94725eea9fc22d2a4
sadia sehil <sadiasehil89@gmail.com>
2018-09-09 10:41:12 -0700

    fibonacci

--- src/math/problems/Fibonacci.java
+++ src/math/problems/Fibonacci.java
@@ -3,9 +3,20 @@ package math.problems;
 public class Fibonacci {
     public static void main(String[] args) {
          /*
-          Write 40 Fibonacci numbers with java.
-         */
+          Write 40 Fibonacci numbers with java.*/
 
+        int n=40;
+        int i = 0;
+        int j = 1;
+        int temp;
+        System.out.println(" The 40 Fibonacci numbers: ");
+        for (int k = 0; k < n; k++)
+        {
+            System.out.println(i+",");
+        temp = i + j;
+        i = j;
+        j = temp;
+        }
 
-    }
 }
+        }
