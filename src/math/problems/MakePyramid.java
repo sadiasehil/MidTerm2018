691b88a11d13275648352faaf8216212941fbd46
sadia sehil <sadiasehil89@gmail.com>
2018-09-10 22:31:26 -0700

    pyramid

--- src/math/problems/MakePyramid.java
+++ src/math/problems/MakePyramid.java
@@ -13,7 +13,34 @@ public class MakePyramid {
                          * * * * * *
 
         */
+        int n = 100;
+        printPyramid(n);
+    }
+    public static void printPyramid(int n)
+    {
+        // number of spaces
+        int k = 2*n - 2;
 
+        for (int i=0; i<n; i++)
+        {
 
+            for (int j=0; j<k; j++)
+            {
+                // printing spaces
+                System.out.print(" ");
+            }
+
+            // decrementing k after each loop
+            k = k - 1;
+
+            for (int j=0; j<=i; j++ )
+            {
+                // printing stars
+                System.out.print("* ");
+            }
+
+            // ending line after each row
+            System.out.println();
         }
+    }
   }
